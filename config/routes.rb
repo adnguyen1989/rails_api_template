0e295768fbe16af2020f365bea2d80173c7388c5
Anh Nguyen <adnguyen1989@gmail.com>
2016-02-10 17:53:08 +0700

    add doorkeeper to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   devise_for :users, :skip => :sessions, controllers: {registrations: 'api/registrations', passwords: 'api/passwords', confirmations: 'api/confirmations'}, defaults: { format: :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  use_doorkeeper do
+    skip_controllers :applications, :authorized_applications, :authorizations
+    # controllers :tokens => 'api/tokens'
+  end
+
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
   #
