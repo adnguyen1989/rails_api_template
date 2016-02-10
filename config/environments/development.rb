34cb80d914c8c326c17cf7a5951db58c7d50a438
Anh Nguyen <adnguyen1989@gmail.com>
2016-02-10 16:58:11 +0700

    install gem devise and configure mailer in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,11 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
+  config.action_mailer.perform_deliveries = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
