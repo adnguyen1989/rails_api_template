33e2c4f52fee9baf703281714e01078801da0a2b
Anh Nguyen <adnguyen1989@gmail.com>
2016-02-10 21:55:45 +0700

    remove devise files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
