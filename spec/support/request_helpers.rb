abaa0bfa98d9701107e57bb6dcfedde272a08b70
Anh Nguyen <adnguyen1989@gmail.com>
2016-02-10 17:25:49 +0700

    add versioning in route, editted spec helpers and added helpers under spec support v2

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -7,7 +7,7 @@ module Request
 
   module HeadersHelpers
     def api_header(version = 1)
-      request.headers['Accept'] = "applicatin/vnd.yomiap.v#{version}"
+      request.headers['Accept'] = "v#{version}"
     end
 
     def api_response_format(format = Mime::JSON)
