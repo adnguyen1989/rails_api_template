80a4e185088f2b37e43389840994d292cabb5ba1
Anh Nguyen <adnguyen1989@gmail.com>
2016-02-10 17:49:32 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => :sessions, controllers: {registrations: 'api/registrations', passwords: 'api/passwords', confirmations: 'api/confirmations'}, defaults: { format: :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
